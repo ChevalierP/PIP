9a5ea0b5cfdb8bbcb406491f6af602636d345ad2
ChevalierP <chevalierp01@gmail.com>
2015-05-12 00:31:03 +0200

    Log
    path length

--- Tests/treasureMap/TM_Qlearning/TM_Qlearning/TM_Qlearning.cpp
+++ Tests/treasureMap/TM_Qlearning/TM_Qlearning/TM_Qlearning.cpp
@@ -54,7 +54,7 @@ std::pair<double,int> maxOfRow(Eigen::Matrix<double, m1size, m1size> M, int stat
 
 int main()
 {
-	std::ofstream file("log.txt");
+	std::ofstream file("log.csv");
 	Eigen::Matrix<double, m1size, m1size> Q1; //exploration
 	Q1.setConstant(0);
 	Eigen::Matrix<double, m1size, m1size> Q2; //exploitation
@@ -65,9 +65,23 @@ int main()
 	Q4.setConstant(0);
 	int state1 = 0, state2 = 0, state3 = 0, state4 = 0;
 	int action1 = 0, action2 = 0, action3 = 0, action4 = 0;
+	int compteur1 = 0;
+	int compteur2 = 0;
+	int compteur3 = 0;
+	int compteur4 = 0;
+	std::vector<int> conv1;
+	std::vector<int> conv2;
+	std::vector<int> conv3;
+	std::vector<int> conv4;
 
-	for (int i = 0; i < 100000; i++)
+	for (int i = 0; i < 1000000; i++)
 	{
+		
+		compteur1++;
+		compteur2++;
+		compteur3++;
+		compteur4++;
+
 		std::uniform_real_distribution<> dis(0, 1);
 		double r = dis(gen);
 
@@ -87,17 +101,52 @@ int main()
 		state3 = action3;
 		state4 = action4;
 		if (state1 == 24)
+		{
 			state1 = 0;
+			conv1.emplace_back(compteur1);
+			compteur1 = 0;
+		}
 		if (state2 == 24)
-			state2 = 0;
+		{
+			state2 = 0; 
+			conv2.emplace_back(compteur2);
+			compteur2 = 0;
+		}
 		if (state3 == 24)
+		{
 			state3 = 0;
+			conv3.emplace_back(compteur3);
+			compteur3 = 0;
+		}
 		if (state4 == 24)
+		{
 			state4 = 0;
+			conv4.emplace_back(compteur4);
+			compteur4 = 0;
+		}
 	}
-	file << Q1 << std::endl;
-	file.close();
+	for (int i = 0; i < conv1.size(); i++)
+	{
+		file << conv1[i] << std::endl;
+	}
+	file << '\t' << std::endl;
+	for (int i = 0; i < conv2.size(); i++)
+	{
+		file << conv2[i] << std::endl;
+	}
+	file << '\t' << std::endl;
+	for (int i = 0; i < conv3.size(); i++)
+	{
+		file << conv3[i] << std::endl;
+	}
+	file << '\t' << std::endl;
+	for (int i = 0; i < conv4.size(); i++)
+	{
+		file << conv4[i] << std::endl;
+	}
 
+	file.close();
+	/*
 	Eigen::Matrix<char, 5, 5> test;
 	test.setConstant('.');
 	int state = 0;
@@ -107,9 +156,9 @@ int main()
 		std::cout << test << std::endl << std::endl;
 		std::cin.get();
 		if(state == 24) break;
-		state = maxOfRow(Q3, state).second;
+		state = maxOfRow(Q2, state).second;
 	}
-
+	*/
 	system("pause");
 	return 0;
 }
