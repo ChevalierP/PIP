d6ed3d8144e8f86b0f4b649f2cccee1cb5988ea2
Eisenheim <khan.temujin@gmail.com>
2015-06-17 14:57:49 +0200

    Added Config.h

---
+++ src/OptimisationTrajectoire/OptimisationTrajectoire/Config.h
@@ -0,0 +1,30 @@
+#ifndef _PIP_CONFIG_H_
+#define _PIP_CONFIG_H_
+
+#define CONFIG_VAR(T, name) \
+	public: \
+		T name() const { return m_##name; } \
+		Config& name(T name##_) { m_##name = name##_; return *this; } \
+	private: \
+		T m_##name;
+
+class Config
+{
+	CONFIG_VAR(float, gamma);
+	CONFIG_VAR(float, alpha);
+	CONFIG_VAR(float, steeringCostFactor);
+	CONFIG_VAR(int, attemptCount);
+	CONFIG_VAR(int, maxIterationCount);
+	CONFIG_VAR(float, finishLineReward);
+	CONFIG_VAR(float, offTrackCost);
+	CONFIG_VAR(float, skidCost);
+	CONFIG_VAR(bool, rewardFinish);
+	CONFIG_VAR(bool, rewardSkid);
+	CONFIG_VAR(bool, rewardOffTrack);
+	CONFIG_VAR(bool, rewardSteeringChange);
+	CONFIG_VAR(bool, rollingStart);
+	CONFIG_VAR(float, coefficientOfFriction);
+	CONFIG_VAR(float, accelerationOfGravity);
+};
+
+#endif // !_PIP_CONFIG_H_
